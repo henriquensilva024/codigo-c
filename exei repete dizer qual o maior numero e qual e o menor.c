191e49d1172cfa675193cc6826fb6a9f0fd125aa
henrique silva <37378548+henriquensilva024@users.noreply.github.com>
2018-03-17 17:43:27 -0300

    Rename exei repete.c to exei repete dizer qual o maior numero e qual e o menor.c

--- exei repete dizer qual o maior numero e qual e o menor.c
+++ exei repete dizer qual o maior numero e qual e o menor.c
@@ -17,35 +17,35 @@ main(){
     printf("digite o valor de e: ");
     scanf("%d",&e);
     if(a>b&&a>c&&a>d&&a>e){
-            printf("\nA é o maior numero: %d\n",a);
+            printf("\nA Ã© o maior numero: %d\n",a);
     }
     if(b>a&&b>c&&b>d&&b>e){
-            printf("\nB é o maior numero: %d\n",b);
+            printf("\nB Ã© o maior numero: %d\n",b);
     }
     if(c>a&&c>b&&c>d&&c>e){
-            printf("\nc é o maior numero: %d\n",c);
+            printf("\nc Ã© o maior numero: %d\n",c);
     }
     if(d>a&&d>b&&d>c&&d>e){
-            printf("\nD é o maior numero: %d\n",d);
+            printf("\nD Ã© o maior numero: %d\n",d);
     }
     if(e>a&&e>b&&e>c&&e>d){
-            printf("\nE é o maior numero: %d\n",e);
+            printf("\nE Ã© o maior numero: %d\n",e);
 
     }
     if(a<b&&a<c&&a<d&&a<e){
-            printf("A é o menor numero: %d\n\n",a);
+            printf("A Ã© o menor numero: %d\n\n",a);
     }
     if(b<a&&b<c&&b<d&&b<e){
-            printf("B é o menor numero: %d\n\n",b);
+            printf("B Ã© o menor numero: %d\n\n",b);
     }
     if(c<a&&c<b&&c<d&&c<e){
-            printf("c é o menor numero: %d\n\n",c);
+            printf("c Ã© o menor numero: %d\n\n",c);
     }
     if(d<a&&d<b&&d<c&&d<e){
-            printf("D é o menor numero: %d\n\n",d);
+            printf("D Ã© o menor numero: %d\n\n",d);
     }
     if(e<a&&e<b&&e<c&&e<d){
-            printf("E é o menor numero: %d\n\n",e);
+            printf("E Ã© o menor numero: %d\n\n",e);
 
     }
     }
